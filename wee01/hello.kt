32dbb68e691b760ad69277e11721ffcb3e1475bc
Junior811 <123098109+Junior811@users.noreply.github.com>
2023-01-24 09:49:18 -0600

    Update hello.kt

--- wee01/hello.kt
+++ wee01/hello.kt
@@ -1,1 +1,10 @@
 
+/**
+* First Kotlin file / Luis Ledezma
+*/
+
+fun main() {
+    println("Hello, world!!!")
+    println("I am learning Kotlin.")
+    println("rainy days and Mondays always get me down")
+}
