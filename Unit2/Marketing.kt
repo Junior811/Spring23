6f30cfbdcc7a8aa485a0372b8b5823014ebbcbcb
Junior811 <123098109+Junior811@users.noreply.github.com>
2023-02-12 23:40:47 -0600

    Create Marketing.kt

---
+++ Unit2/Marketing.kt
@@ -0,0 +1,13 @@
+fun main() {
+  
+    var friendsCount=501
+    
+   if(friendsCount > 500){
+   System.out.println(" Adopt a Dog Today!")
+}
+else{
+    System.out.println("Cat Food At 20% Off")
+    
+}
+
+}
