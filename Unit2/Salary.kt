73faa24f4ee4e284c74e77ed3b2749e205af874e
Junior811 <123098109+Junior811@users.noreply.github.com>
2023-02-12 23:49:23 -0600

    Create Salary.kt

---
+++ Unit2/Salary.kt
@@ -0,0 +1,7 @@
+fun main() {
+  var name= "Luis Ledezma"
+    var age= 19
+    var annualPay= 10000.0
+    System.out.print("My name is "+name +", my age is " +age+" and I hope to earn $ "+annualPay+ " per year.")
+
+}
